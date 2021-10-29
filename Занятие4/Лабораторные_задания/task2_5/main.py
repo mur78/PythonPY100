31576aa4c0e71987db96ae5d075fa378aba05761
Mur78 <drivem78@mail.ru>
2021-10-29 16:04:11 +0300

    update 28.10

--- Занятие4/Лабораторные_задания/task2_5/main.py
+++ Занятие4/Лабораторные_задания/task2_5/main.py
@@ -1,4 +1,13 @@
+def poliand():
+    n = int(input("Введите число: "))
+    s = str(n)
+    reverse_s = ''.join(s[::-1])
+    if s == reverse_s:
+        print("Палиндром")
+    else:
+        print("Нет")
+
 if __name__ == "__main__":
     # Write your solution here
-    pass
+    poliand()
 
